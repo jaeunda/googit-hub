ee19d48b918e7fec5448a0bb53b75447d023f8d8
daeun <jaeunda@gmail.com>
2025-09-03 01:35:55 +0900

    feat(header): add commands.h #1

---
+++ include/commands.h
@@ -0,0 +1,11 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+void execute_init(const char *project_num, const char *student_id, const char *class_num);
+void execute_push(const char *msg);
+void execute_run(void);
+void execute_zip(int is_revert);
+void execute_test(void);
+void execute_restore(void);
+
+#endif
