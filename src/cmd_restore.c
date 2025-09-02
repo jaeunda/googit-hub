1c4a8b825a6e28fd27f43b0f4316cda2f87b811c
daeun <jaeunda@gmail.com>
2025-09-03 01:50:49 +0900

    feat(cmd): add execute command skeleton #1

---
+++ src/cmd_restore.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "commands.h"
+
+void execute_restore(void){
+    printf("[RESTORE]\n");
+    return;
+}
---
+++ src/cmd_run.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "commands.h"
+
+void execute_run(void){
+    printf("[RUN]\n");
+    return;
+}
