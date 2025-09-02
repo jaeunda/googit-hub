983474881f42fadfb455e261484372d726002de7
daeun <jaeunda@gmail.com>
2025-09-03 01:33:20 +0900

    feat(main): add execution logic #1

--- src/main.c
+++ src/main.c
@@ -14,17 +14,40 @@ int main(int argc, char **argv){
     char *command = argv[1];
 
     if (!strcmp(command, "init")){
-
+        if (argc != 5){
+            fprintf(stderr, "Error: Incorrect arguments for init.\n");
+            fprintf(stderr, "Usage: googit init <project_num> <student_id> <class_num>");
+            exit(1);
+        }
+        execute_init(argv[2], argv[3], argv[4]);
     } else if (!strcmp(command, "push")){
-
+        if (argc == 4 && !strcmp(argv[2], "-m")){
+            execute_push(argv[3]);
+        } else if (argc == 2){
+            execute_push(NULL);
+        } else {
+            fprintf(stderr, "Error: Incorrect arguments for push.\n");
+            fprintf(stderr, "Usage: googit push [-m \"message\"]\n");
+            exit(1);
+        }
     } else if (!strcmp(command, "run")){
-
+        execute_run();
     } else if (!strcmp(command, "zip")){
-
+        if (argc == 3 && !strcmp(argv[2], "--revert")){
+            execute_zip(1);
+        } else if (argc == 2){
+            execute_zip(0);
+        } else {
+            fprintf(stderr, "Error: Incorrect arguments for zip.\n");
+            fprintf(stderr, "Usage: googit zip [--revert]\n");
+            exit(1);
+        }
     } else if (!strcmp(command, "test")){
-
+        execute_test();
     } else if (!strcmp(command, "restore")){
-
+        execute_restore();
+    } else if (!strcmp(command, "--help") || !strcmp(command, "help")){
+        print_usage();
     } else {
         fprintf(stderr, "Error: Unknown command %s\n", command);
         print_usage();
