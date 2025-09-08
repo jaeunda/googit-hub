58cbe7d9dd2705573c9dda3f09591484812a4b30
daeun <jaeunda@gmail.com>
2025-09-09 01:39:39 +0900

    feat(push): implement push logic #5

--- src/cmd_push.c
+++ src/cmd_push.c
@@ -7,8 +7,10 @@
 #include "commands.h"
 #include "utils.h"
 
+static int backup_to_gdrive(const char *msg);
 void execute_push(const char *msg){
     char sync_command[1024];
+    // 1. copy to output_dir
     snprintf(sync_command, sizeof(sync_command), 
         "rsync -a --delete --compare-dest=.googit/original/ xv6-public/ .googit_output_dir/");
     
@@ -16,6 +18,11 @@ void execute_push(const char *msg){
         fprintf(stderr, "Error: Failed to sync modified files.\n");
         exit(EXIT_FAILURE);
     }
+    // 2. backup output_dir to Google Drive
+    if (backup_to_gdrive(msg) < 0){
+        fprintf(stderr, "Error: Failed to backup to Google Drive\n");
+        exit(EXIT_FAILURE);
+    }
 
-
+    printf("\nPush completed.\n");
 }
