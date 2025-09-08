02e2e59c565b9cbfc69dcfbaa9a811269848ef29
daeun <jaeunda@gmail.com>
2025-09-09 00:52:27 +0900

    feat(push): implement sync modified files to output_dir #5

--- src/cmd_push.c
+++ src/cmd_push.c
@@ -2,10 +2,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
 #include "commands.h"
+#include "utils.h"
 
 void execute_push(const char *msg){
-    printf("[PUSH] %s\n", msg);
-    return;
+    char sync_command[1024];
+    snprintf(sync_command, sizeof(sync_command), 
+        "rsync -a --delete --compare-dest=.googit/original/ xv6-public/ .googit_output_dir/");
+    
+    if (run_command(sync_command) != 0){
+        fprintf(stderr, "Error: Failed to sync modified files.\n");
+        exit(EXIT_FAILURE);
+    }
+
+
 }
