b5c447318c47111b318e6f7d4dfda296e251751a
daeun <jaeunda@gmail.com>
2025-09-10 00:51:03 +0900

    feat(zip): create filename #9

--- src/cmd_zip.c
+++ src/cmd_zip.c
@@ -30,6 +30,28 @@ void execute_zip(int is_revert){
     }
 
     // 1. make filename
+    char project_num[8];
+    char student_id[16];
+    char class_num[8];
+    char version_str[8];
+
+    if (read_config("project_num", project_num, sizeof(project_num)) < 0 ||
+        read_config("student_id", student_id, sizeof(student_id)) < 0 ||
+        read_config("class_num", class_num, sizeof(class_num)) < 0 ||
+        read_config("version", version_str, sizeof(version_str)) < 0){
+        fprintf(stderr, "Error: Could not read data from config file.\n");
+        exit(EXIT_FAILURE);
+    }
+    int version = atoi(version_str);
+
+    char filename[256];
+    if (!version){
+        snprintf(filename, sizeof(filename), "#%s_%s_%s.zip",
+            project_num, student_id, class_num);
+    } else {
+        snprintf(filename, sizeof(filename), "#%s_%s_%s_V%d.zip",
+            project_num, student_id, class_num, version+1);
+    }
 
     // 2. zip
 
