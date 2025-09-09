dd25cae578d609e3842e454c4ad079e7c7bd888a
daeun <jaeunda@gmail.com>
2025-09-09 16:06:11 +0900

    feat(zip): implement revert option #9

--- src/cmd_zip.c
+++ src/cmd_zip.c
@@ -4,12 +4,34 @@
 #include <string.h>
 
 #include "commands.h"
+#include "utils.h"
 
 void execute_zip(int is_revert){
+    // 0. --revert
     if (is_revert){
-        printf("[ZIP] Version Down\n");
-    } else {
-        printf("[ZIP] Version UP\n");
+        char version_str[8];
+        if (read_config("version", version_str, sizeof(version_str)) < 0){
+            fprintf(stderr, "Error: Could not read config file.\n");
+            exit(EXIT_FAILURE);
+        }
+
+        int version = atoi(version_str);
+        if (version > 0){
+            version--;
+            snprintf(version_str, sizeof(version_str), "%d", version);
+            if (write_config("version", version_str) < 0){
+                fprintf(stderr, "Error: Could not update version.\n");
+                exit(EXIT_FAILURE);
+            }
+            printf("Version reverted to %d.\n", version);
+        } else {
+            printf("Version is already 0.\n");
+        }
     }
-    return;
+
+    // 1. make filename
+
+    // 2. zip
+
+    // 3. version++
 }
